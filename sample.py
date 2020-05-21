1b80fa29d3650a58d525d6f685ef6e3c46934509
arithmetic1728 <liusijun1985@gmail.com>
2020-05-20 17:28:52 -0700

    update sample.py

--- sample.py
+++ sample.py
@@ -7,34 +7,14 @@ import google.oauth2.credentials
 
 
 # Fill in your project_id.
-project_id = "<YOUR_PROJECT_ID>"
+project_id = "<FILL IN PROJECT ID>"
 
 
 def list_key_rings():
     # Get the user access token.
     cred = google.oauth2.credentials.UserAccessTokenCredentials()
 
-    # If device client certificate exists, it will be used to establish mutual TLS connection,
-    # and the KMS client will automatically switch to KMS mtls endpoint.
-    # If device client certificate doesn't exists, mutual TLS connection will not be established,
-    # and the default mtls endpoint will be used.
-    # The existence of device client certificate can be checked via
-    # 'mtls.has_default_client_cert_source()'. You can call 'mtls.default_client_cert_source()'
-    # to get a callback, which produces the client certificate on execution.
-    if mtls.has_default_client_cert_source():
-        print(
-            "Default client cert source is found. It will be used to create mutual TLS channel"
-        )
-        callback = mtls.default_client_cert_source()
-    else:
-        print(
-            "Default client cert source is not found. Mutual TLS channel will not be created"
-        )
-        callback = None
-
-    # Pass the callback to ClientOptions, and pass the ClientOptions to KeyManagementServiceClient.
-    client_options = ClientOptions.ClientOptions(client_cert_source=callback)
-    client = KeyManagementServiceClient(credentials=cred, client_options=client_options)
+    client = KeyManagementServiceClient(credentials=cred)
 
     # Lists keys in the "global" location.
     parent = "projects/{project}/locations/{location}".format(
